cfda98a37f5df62d5b88eacfc46503bbcc0dceb8
Weverton <wevertonbezzera013@gmail.com>
2021-02-22 18:35:14 -0300

    bootstrap

--- melhoresdrinks/src/App.js
+++ melhoresdrinks/src/App.js
@@ -2,7 +2,28 @@ import React from 'react';
 
 function App() {
   return (
-    <h1>MEU APP</h1>
+
+    <section>
+      <nav class="navbar navbar-dark bg-dark">
+        
+      </nav>
+
+      <nav class="navbar navbar-dark bg-primary">
+        
+      </nav>
+
+      <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+        
+      </nav>
+
+
+      <form>
+        <input type="text" placeholder="Digite seu nome completo"></input>
+        <input type="text" placeholder="Digite seu E-mail"></input>
+        <textarea placeholder="Digite seu texto"></textarea>
+        <button>Enviar</button>
+      </form>
+    </section>
   );
 }
 
