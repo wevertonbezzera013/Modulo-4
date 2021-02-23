1e825e763c4c17636a3b6648f162b87e4ec0acea
Weverton <wevertonbezzera013@gmail.com>
2021-02-22 23:26:01 -0300

    forms

--- bonsdrinks/src/App.js
+++ bonsdrinks/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import React from 'react';
 import NavBar from './components/NavBar/NavBar'
+import Forms from './components/Forms/forms'
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Forms/>
     </div>
   );
 }
---
+++ bonsdrinks/src/components/Forms/forms.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './forms.css'
+
+class Forms extends React.Component {
+    render() {
+        return(
+            <section>
+                <button className="send-btn">Enviar</button>
+            </section>
+        )
+    }
+}
+
+export default Forms;
