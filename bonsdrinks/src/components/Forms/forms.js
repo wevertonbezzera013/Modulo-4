431e5119c41eae98f85c08f0da8b9709c866acf7
Weverton <wevertonbezzera013@gmail.com>
2021-02-22 23:54:40 -0300

    forms

--- bonsdrinks/src/components/Forms/forms.js
+++ bonsdrinks/src/components/Forms/forms.js
@@ -5,8 +5,12 @@ class Forms extends React.Component {
     render() {
         return(
             <section>
+                <input id="firstName" type="text" placeholder="Nome completo"></input>
+                <input id="lastName" type="text" placeholder="E-mail"></input>
+                <textarea type="text" placeholder="Escreva seu texto aqui."></textarea>
                 <button className="send-btn">Enviar</button>
             </section>
+            
         )
     }
 }
