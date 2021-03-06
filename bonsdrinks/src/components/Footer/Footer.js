ce766f42af03e128836d8e3ba9172e2b0819c31f
Weverton <wevertonbezzera013@gmail.com>
2021-03-05 21:11:07 -0300

    footer

---
+++ bonsdrinks/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Footer.css'
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                <p>Feito com ❤️ por <a href="https://github.com/wevertonbezzera013">Weverton</a>!</p>
+            </footer>
+        )
+    }
+}
+
+export default Footer;
--- bonsdrinks/src/components/Pages/Contact.js
+++ bonsdrinks/src/components/Pages/Contact.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavBar from '../NavBar/NavBar';
 import Forms from '../Forms/forms';
+import Footer from '../Footer/Footer';
 
 function Contact() {
   return (
     <div className="App">
         <NavBar/>
         <Forms/>
+        <Footer/>
     </div>
   );
 }
